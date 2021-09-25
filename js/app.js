222a9c959c502d3c96d609ec8dca666b543dcc17
Nicoli1977 <86806874+Nicoli1977@users.noreply.github.com>
2021-09-25 15:37:55 -0500

    techdegree-project-5

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 
 baguetteBox.run('.gallery');
 
-const search = new Filter('search','data-caption');
+
+
+let search = document.querySelector('#search');
+let legends = document.querySelectorAll('[data-caption]');
+
+let searchBar = event => {
+    let searchTerm = event.target.value.toLowerCase();
+    legends.forEach(legends => {
+        let text = legends.getAttribute('data-caption').toLowerCase();
+
+        if(text.includes(searchTerm)) {
+            legends.style.display = "block";
+        } else {
+            legends.style.display = "none";
+        }
+    });
+}
+
+search.addEventListener('keyup', searchBar);
